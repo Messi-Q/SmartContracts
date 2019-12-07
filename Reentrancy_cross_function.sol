d0a8856cfde86ccc2db86be3b9157cbe3ea67e54
Messi-Q <messi.qp711@gmail.com>
2019-12-07 16:10:32 +0800

    Create Reentrancy_cross_function.sol

---
+++ Reentrancy_cross_function.sol
@@ -0,0 +1,31 @@
+contract Victim {
+  mapping (address => uint) private Balance;
+  uint bonuspool;
+  
+  function getBonus(address rept) {
+    if(bonusPool > 0) {
+      rept.call.value(bonus)();
+      bonuspool -= bonus;
+    }
+  }
+  
+  function withdraw(uint amount) {
+    msg.sender.call.value(amount)();
+    Balance[msg.sender] -= amount;
+  }
+}
+
+contract Attacker {
+  ...
+  function Attacker() {
+    victim = Victim(vic_addr);
+  }
+  function attack() {
+    victim.getBonus(_owner);
+  }
+  function() payable {
+    if (count++ < 10) {
+      victim.withdraw(2 ether);
+    }
+  }
+}
